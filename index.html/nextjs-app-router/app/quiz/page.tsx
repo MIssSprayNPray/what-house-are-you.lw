411290f3ae364533b98168905c703ec975233620
MIssSprayNPray <bailey.n.feltner@gmail.com>
2025-08-07 18:29:26 -0500

    Delete index.html directory

--- index.html/nextjs-app-router/app/quiz/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import dynamic from "next/dynamic";
-
-const HouseQuiz = dynamic(() => import("../../components/HouseQuiz"), { ssr: false });
-
-export default function Page() {
-  return <HouseQuiz />;
-}
